a42f4ec817dad107db677951116c8040a4bf46bc
Егор Старцев <startseveg@gmail.com>
2024-02-19 16:26:08 +0300

    последняя задача

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" 
+                    href="{url_for('static', filename='css/style.css')}" />
+                    <title>Пейзажи Марса</title>
+                  </head>
+                  <body>
+                    <div>
+                        <h1>Пейзажи Марса</h1>
+                    </div>
+                    <div class="container">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                          <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                          </ol>
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="static/img/1.jpg" class="d-block w-100" alt="изображение не найдено">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="static/img/2.jpeg" class="d-block w-100" alt="изображение не найдено">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="static/img/3.jpg" class="d-block w-100" alt="изображение не найдено">
+                            </div>
+                          </div>
+                          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" 
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" 
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
+                    </div>
+                  </body>
+                </html>"""
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
